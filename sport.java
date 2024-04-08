888c63d84b8bdfb9f7649b40d49b29cba41e300b
dangolprajwol1 <125624510+dangolprajwol1@users.noreply.github.com>
2024-04-08 09:45:19 -0700

    Update sport.java

--- sport.java
+++ sport.java
@@ -1,25 +1,9 @@
-package com.pa.collectionsapi;
+
 
 import java.util.ArrayList;
 import java.util.Scanner;
 
-//@FunctionalInterface
-//public interface MF {
-//    public Integer sqr(int a);
-//
-//    default String sqr(String a) {
-//        return a + a;
-//    }
-//}
-// class Test {
-//    public static void main(String[] args) {
-//        MF fi = n -> {
-//            return n * n;
-//        };
-//        System.out.println(fi.sqr(5));
-//
-//    }
-//}
+
 interface Sport{
     public void calculateAvgAge(int[] age);
     public void retirePlayer(int playerId);
